ee57d983c3cbec54e9ec63b8ffcdbafa458be040
Maurício Generoso <mauro_generoso@hotmail.com>
2018-04-04 00:17:47 -0300

    ..

--- backend/loader.js
+++ backend/loader.js
@@ -1,6 +1,3 @@
-// Não esquecer de iniciar o mongo, pois se não estiver iniciado ocorrerá erro ao iniciar o servidor.
-// No terminal iniciar o servidor do banco com "mongod"
-
 const server = require('./config/server')
 require('./config/database')
 require('./config/routes')(server)
